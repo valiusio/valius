d28b79677a39e1818f23ddae8218d58761630a77
sotiris <sotirispinakoulas@gmail.com>
2019-02-08 15:15:38 +0200

    fix deploy script

--- DEPLOY_PRODUCTION_SCRIPT.sh
+++ DEPLOY_PRODUCTION_SCRIPT.sh
@@ -3,7 +3,7 @@
 sudo service apache2 stop &&
 sudo git pull &&
 sudo composer install &&
-sudo cd frontend/valius/ &&
+ cd frontend/valius/ &&
 sudo npm i &&
 sudo npm run build &&
 sudo service apache2 start
